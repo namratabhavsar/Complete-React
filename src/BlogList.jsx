1bf94d2a4decc273ef03a3387a3ecceafd57efe9
namrata bhavsar <namrata.bhavsar@senecacollege.ca>
2024-12-24 12:07:38 -0500

    Reuse components and passing props into componenets

---
+++ src/BlogList.jsx
@@ -0,0 +1,18 @@
+const BlogList= ({title,blogsprop}) => {
+
+    const blogs = blogsprop;
+   
+    return ( 
+       <div className="blog-list">
+        <h2>{title}</h2>
+        {blogs.map((item) =>(
+                <div className="blog-preview" key={item.id}>
+                    <h2>{item.title}</h2>
+                    <p>written by  {item.author}</p>
+                </div>
+            ))}
+       </div>
+     );
+}
+ 
+export default BlogList;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,36 +1,22 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
 
-    //let nametest = "mario";
-    //Events
-    const handleClick = () => {
-        console.log("Clicked");
-        SetName("Jane");
-        SetAge(27);
-    }
-    const handleClickMe = (e) => {
-        console.log("Clicked", e.target);
-    }
-    const handleClickAgain = (name,e) =>{
-        console.log(name,e.target);
-    }
+       //Outputting List Example
+       const[blogs,SetBlogs] = useState([
+        {title:'Computer Blog', body:'Computer Description....',author:'Namrata',id:1 },
+        {title:'Electrical Blog', body:'Electrical Description....',author:'Yoshi',id:2 },
+        {title:'Civil Blog', body:'Civil Description....',author:'Jane',id:3 },
+        {title:'Business Blog', body:'Business Description....',author:'Mario',id:4 },
+        {title:'Finance Blog', body:'Finance Description....',author:'Namrata',id:5 }
+    ])
 
-    //State 
-    const[nametest, SetName]= useState("mario");
-    const[age,SetAge] = useState(25);
     return ( 
-    
         <div className="home">
-           <h1>Home Page</h1>
-           <p>{nametest}</p>
-           <p>{age}</p>
-           <button onClick={handleClick}>Click Me</button>
-           <button onClick={(e) => handleClickMe(e)}>Click Me , I have EventArg</button>
-           <button onClick={(e) => {handleClickAgain("namrata",e)}}>Click To Get Name</button>
+           <BlogList blogsprop={blogs} title= {"All Blogs !"}/>
+           <BlogList blogsprop={blogs.filter((item) => item.author==='Namrata')} title= {"Namrata's Blogs !"}/>
         </div>
-
-        
      );
 }
  
